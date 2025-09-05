c023a792fbe7a5c8a192d9b7d0ff9be73a737bc5
nahlaishfaq2005 <nahlaishfaq@gmail.com>
2025-09-05 09:33:33 +0530

    add all css into one and home dashboard

---
+++ php/adminhome.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>User's Home Page</title>
+        <link rel="stylesheet" href="../new css/main.css">  
+        <link rel="stylesheet" href="../css/userhome.css">
+</head>
+<body>
+
+<nav class="navbar glass-container">
+    
+<img src="../images/logo.png" alt="Logo" class="logo">
+
+
+<ul>
+    <li>Users</li>
+    <li>Community Dashboard</li>
+    <!-- <li>profile settings</li> -->
+    <li>Logout</li>
+    <li>                  </li>
+    
+</ul>
+</nav>
+
+
+</body>
+
+</html>
